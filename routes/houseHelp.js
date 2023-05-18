0fc47f094d7847a9a0d2da21051355bfe1eb65d1
ankit sejwal <ankit@sejw.al>
2023-05-18 12:55:24 +0000

    find a househelp

--- routes/houseHelp.js
+++ routes/houseHelp.js
@@ -4,6 +4,17 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {});
 
+// get a house help
+router.get('/:id', async (req, res) => {
+  try {
+    const houseHelp = await HouseHelp.findById(req.params.id);
+    if (!houseHelp) res.status(404).send('house help not found');
+    res.status(200).send(houseHelp);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
 // add a new house help
 router.post('/', async (req, res) => {
   let houseHelp = new HouseHelp({
