8757446f8697e75e24af4a67492af102dcc8d358
ankit sejwal <ankit@sejw.al>
2023-05-18 14:44:02 +0000

    get a security guard

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use('/api/users', users);
 app.use('/api/visitors', visitors);
 app.use('/api/househelp', houseHelp);
-app.use('/api/securityguard', securityGuard);
+app.use('/api/securityguards', securityGuard);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on ${port}`));
--- routes/securityGuards.js
+++ routes/securityGuards.js
@@ -2,6 +2,17 @@ const express = require('express');
 const SecurityGuard = require('../models/securityGuards');
 const router = express.Router();
 
+// get a security guard
+router.get('/:id', async (req, res) => {
+  try {
+    const securityGuard = await SecurityGuard.findById(req.params.id);
+    if (!securityGuard) res.status(404).send('security guard not found');
+    res.status(200).send(securityGuard);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
 // add new security guard
 router.post('/', async (req, res) => {
   let securityGuard = new SecurityGuard({
