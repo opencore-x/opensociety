c567298a163252eb24f514d598baf814fa8eb3de
ankit sejwal <ankit@sejw.al>
2023-05-18 14:21:30 +0000

    added security model

---
+++ models/security.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const config = require('config');
+
+mongoose
+  .connect(
+    `mongodb+srv://${config.get('MONGO_USERNAME')}:${config.get(
+      'MONGO_PASSWORD'
+    )}@vidly.zivn542.mongodb.net/vidly`
+  )
+  .then(() => console.log('connected to mongodb'))
+  .catch((err) => console.log(err));
+
+const securitySchema = new mongoose.Schema({
+  firstName: { type: String, required: true, min: 3, max: 20 },
+  middleName: { type: String, min: 3, max: 20 },
+  lastName: { type: String, required: true, min: 3, max: 20 },
+  phone: { type: String, required: true, min: 9, max: 10 },
+  password: { type: String, required: true, max: 40 },
+});
+
+module.exports = mongoose.model('Security', securitySchema);
