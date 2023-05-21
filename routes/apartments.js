33c4d33a7919ec53a36af5c579fd623df1663442
ankit sejwal <ankit@sejw.al>
2023-05-21 09:50:49 +0000

    edit an apartment

--- routes/apartments.js
+++ routes/apartments.js
@@ -19,4 +19,24 @@ router.post('/', async (req, res) => {
   res.status(200).send(apartment);
 });
 
+// update an apartment
+router.put('/:id', async (req, res) => {
+  let apartment = {
+    name: req.body.name,
+    owner: req.body.owner,
+    tenant: req.body.tenant,
+    occupants: req.body.occupants,
+  };
+
+  const { value, error } = validateApartment(apartment);
+  if (error) return res.status(500).send(error.details[0].message);
+  try {
+    apartment = await Apartment.findByIdAndUpdate(req.params.id, value, { new: true });
+    if (!apartment) return res.status(404).send('apartment not found');
+    res.status(200).send(apartment);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
 module.exports = router;
