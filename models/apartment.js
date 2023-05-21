79acf436268564a96314f99caa884178497ef414
ankit sejwal <ankit@sejw.al>
2023-05-21 06:42:45 +0000

    added joi validation

--- models/apartment.js
+++ models/apartment.js
@@ -15,12 +15,21 @@ const apartmentSchema = new mongoose.Schema({
   name: { type: String, required: true },
   owner: { type: String, min: 3, max: 20 },
   tenant: { type: String, min: 3, max: 20 },
-  residents: [String],
+  occupants: [String],
 });
 
 const Apartment = mongoose.model('Apartment', apartmentSchema);
 
-module.exports = { Apartment, apartmentSchema };
+function validateApartment(apartment) {
+  const schema = Joi.object({
+    name: Joi.string().required(),
+    owner: Joi.string().min(3).max(20),
+    tenant: Joi.string().min(3).max(20),
+    occupants: Joi.array(Joi.string()),
+  });
+}
+
+module.exports = { Apartment, validateApartment };
 
 // todo:
 // change resident to object
