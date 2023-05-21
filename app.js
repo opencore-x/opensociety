7c5ca2efda7f6cfa94c4f37a92f354a2021cd8d6
ankit sejwal <ankit@sejw.al>
2023-05-21 06:56:10 +0000

    add new apartments

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const visitors = require('./routes/visitors');
 const houseHelp = require('./routes/houseHelps');
 const securityGuard = require('./routes/securityGuards');
 const maintenanceRequest = require('./routes/maintenanceRequest');
+const apartments = require('./routes/apartments');
 
 const app = express();
 app.use(express.json());
@@ -13,6 +14,7 @@ app.use('/api/visitors', visitors);
 app.use('/api/househelp', houseHelp);
 app.use('/api/securityguards', securityGuard);
 app.use('/api/maintenancerequest', maintenanceRequest);
+app.use('/api/apartments', apartments);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on ${port}`));
---
+++ routes/apartments.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { Apartment, validateApartment } = require('../models/apartments');
+
+const router = express.Router();
+
+// add a new aparatment
+router.post('/', async (req, res) => {
+  let apartment = {
+    name: req.body.name,
+    owner: req.body.owner,
+    tenant: req.body.tenant,
+    occupants: req.body.occupants,
+  };
+
+  const { value, error } = validateApartment(apartment);
+  if (error) return res.status(500).send(error.details[0].message);
+
+  aparatment = await new Apartment(value);
+  res.status(200).send(aparatment);
+});
+
+module.exports = router;
