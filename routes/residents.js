39b4ffbd738e884e65a47ee3f84634f54e97c74e
ankit sejwal <ankit@sejw.al>
2023-05-21 13:44:06 +0000

    edit resident

--- routes/residents.js
+++ routes/residents.js
@@ -23,4 +23,30 @@ router.post('/', async (req, res) => {
   res.status(200).send(resident);
 });
 
+// edit a resident
+router.put('/:id', async (req, res) => {
+  try {
+    let resident = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      dob: req.body.dob,
+      gender: req.body.gender,
+      phone: req.body.phone,
+      email: req.body.email,
+      apartment: req.body.apartment,
+      status: req.body.status,
+      nationality: req.body.nationality,
+    };
+
+    const { value, error } = validateResident(resident);
+    if (error) return res.status(500).send(error.details[0].message);
+
+    resident = await Resident.findByIdAndUpdate(req.params.id, value, { new: true });
+    if (!resident) return res.status(404).send('resident not found');
+    res.status(200).send(resident);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
 module.exports = router;
