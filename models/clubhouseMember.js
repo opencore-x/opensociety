61e5cd9af9f8ac1ed37c296828a6c5bf262d8591
ankit sejwal <ankit@sejw.al>
2023-05-26 10:24:36 +0000

    enhance validateClubhouseMember function

--- models/clubhouseMember.js
+++ models/clubhouseMember.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
 
 const clubhouseMemberSchema = new mongoose.Schema({
   resident: String,
@@ -8,11 +9,19 @@ const clubhouseMemberSchema = new mongoose.Schema({
 
 const ClubhouseMember = mongoose.model('ClubhouseMember', clubhouseMemberSchema);
 
-function validateClubhouseMember() {
-  const schema = Joi.object({
-    resident: Joi.object(),
+function validateClubhouseMember(member, id = false) {
+  const schema = {
+    resident: Joi.objectId(),
     joinDate: Joi.date().optional(),
-  });
+  };
+
+  if (id) {
+    member.id = id;
+    schema.id = Joi.objectId();
+  }
+
+  const clubhouseMemberSchema = Joi.object(schema);
+  return clubhouseMemberSchema.validate(member);
 }
 
 module.exports = { ClubhouseMember, clubhouseMemberSchema };
