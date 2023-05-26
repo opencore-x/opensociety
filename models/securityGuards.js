3868fb9025b0dab8c71a8379322b7ac39ae03c9e
ankit sejwal <ankit@sejw.al>
2023-05-26 10:42:39 +0000

    improved validateSecurityGuard

--- models/securityGuards.js
+++ models/securityGuards.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
 
 const securitySchema = new mongoose.Schema({
   firstName: { type: String, required: true, min: 3, max: 20 },
@@ -8,17 +9,23 @@ const securitySchema = new mongoose.Schema({
   password: { type: String, required: true, max: 40 },
 });
 
-function validateSecurityGuard(securityGuard) {
-  const schema = Joi.object({
+const SecurityGuard = mongoose.model('SecurityGuard', securitySchema);
+
+function validateSecurityGuard(securityGuard, id = false) {
+  const schema = {
     firstName: Joi.string().min(3).max(20).required(),
     lastName: Joi.string().min(3).max(20).required(),
     phone: Joi.string().length(10).required(),
     password: Joi.string().min(6).max(50).require(),
-  });
+  };
 
-  return schema.validate(securityGuard);
+  if (id) {
+    securityGuard.id = id;
+    schema.id = Joi.objectId();
+  }
+
+  const securitySchema = Joi.object(schema);
+  return securitySchema.validate(securityGuard);
 }
-
-const SecurityGuard = mongoose.model('SecurityGuard', securitySchema);
 
 module.exports = { SecurityGuard, validateSecurityGuard };
