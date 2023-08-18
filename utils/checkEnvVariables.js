7ae4a80077fde48aaa8212700899c77e575985a7
ankitsejwal <ankit@sejw.al>
2023-08-18 17:01:54 +0530

    bug fixes

--- utils/checkEnvVariables.js
+++ utils/checkEnvVariables.js
@@ -1,11 +1,13 @@
-export default function checkForEnvironmentVariables() {
+function checkForEnvironmentVariables() {
   checkEnv('MONGO_CONNECTION_STRING');
   checkEnv('JWT_TOKEN');
 }
 
 function checkEnv(variable) {
-  if (!process.env.variable) {
+  if (!process.env[variable]) {
     console.log(`FATAL ERROR: ${variable} STRING NOT PRESENT`);
     process.exit(1);
   }
 }
+
+module.exports = checkForEnvironmentVariables;
--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
-export default function dbConnect() {
+function dbConnect() {
   mongoose.connect(process.env.MONGO_CONNECTION_STRING);
   mongoose.connection.on('connected', () => console.log('MongoDB connected'));
   mongoose.connection.on('disconnected', () => console.log('MongoDB disconnected'));
-  mongoose.conntection.on('error', (error) => console.log('Error', error));
+  mongoose.connection.on('error', (error) => console.log('Error', error));
 }
+
+module.exports = dbConnect;
