44a9d5460b3d5d79c7540b1e5e8e2162c3493602
ankitsejwal <ankit@sejw.al>
2023-08-24 17:49:18 +0530

    added joiSchema

--- models/Vehicle.js
+++ models/Vehicle.js
@@ -5,11 +5,19 @@ Joi.objectid = require('joi-objectid');
 const vehicleSchema = new mongoose.Schema({
   name: { type: String, required: true },
   vehicleNo: { type: String, required: true },
-  type: [String],
+  type: { type: String, enum: ['car', 'scooter', 'bike', 'pickup'] },
   color: { type: String, required: true },
   apartment: { type: mongoose.Schema.Types.ObjectId, ref: 'Apartment', required: true },
 });
 
+const joiSchema = {
+  name: Joi.string().required(),
+  vehicleNo: Joi.string().required(),
+  type: Joi.string().valid('car', 'scooter', 'bike', 'pickup').required(),
+  color: Joi.string().required(),
+  apartment: Joi.objectid(),
+};
+
 const Vehicle = mongoose.model('Vehicle', vehicleSchema);
 
-module.exports = { Vehicle };
+module.exports = { Vehicle, joiSchema };
