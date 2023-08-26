7d012d80191b765ff8441740451f345b79297803
ankitsejwal <ankit@sejw.al>
2023-08-26 20:14:53 +0530

    added password field

--- models/SecurityGuard.js
+++ models/SecurityGuard.js
@@ -5,7 +5,7 @@ Joi.objectid = require('joi-objectid')(Joi);
 const securitySchema = new mongoose.Schema({
   firstName: { type: String, required: true, min: 3, max: 20 },
   lastName: { type: String, required: true, min: 3, max: 20 },
-  phone: { type: String, required: true, min: 9, max: 10 },
+  phone: { type: String, required: true, min: 9, max: 100 },
   password: { type: String, required: true, max: 40 },
 });
 
@@ -13,7 +13,7 @@ const joiSchema = {
   firstName: Joi.string().min(3).max(20).required(),
   lastName: Joi.string().min(3).max(20).required(),
   phone: Joi.string().length(10).required(),
-  password: Joi.string().min(6).max(50).required(),
+  password: Joi.string().min(6).max(100).required(),
   repeatPassword: Joi.string().valid(Joi.ref('password')).required(),
 };
 
