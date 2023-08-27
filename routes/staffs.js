0d1e2c7e7c4a4f098bf38076da5cf1de5b53ed77
ankitsejwal <ankit@sejw.al>
2023-08-27 23:43:28 +0530

    use lodash to omit password from response

--- routes/staffs.js
+++ routes/staffs.js
@@ -1,6 +1,14 @@
 const router = require('express').Router();
+const _ = require('lodash');
 const { Staff, joiSchema } = require('../models/Staff');
 const validate = require('../middleware/validate');
+
+const sendStaff = (res, staff) => {
+  const staff = _.omit(staff.toObject(), ['password']);
+  res.status(200).json(staff);
+};
+
+const NotFoundResponse = (res) => res.status(404).json({ message: 'staff with given id does not exist' });
 
 router.get('/', async (req, res) => {
   const staffs = await Staff.find();
@@ -9,8 +17,8 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', validate('id'), async (req, res) => {
   const staff = await Staff.findById(req.params);
-  if (!staff) return res.status(400).json({ message: 'staff with given id does not exist' });
-  res.status(200).json(staff);
+  if (!staff) return NotFoundResponse(res);
+  sendStaff(res, staff);
 });
 
 router.post('/', validate(joiSchema), async (req, res) => {
@@ -18,22 +26,22 @@ router.post('/', validate(joiSchema), async (req, res) => {
   if (staff) return res.status(400).json({ message: 'staff already exists' });
   staff = new Staff(req.body);
   await staff.save();
-  res.status(200).json(staff);
+  sendStaff(res, staff);
 });
 
 router.put('/:id', validate('id'), validate(joiSchema), async (req, res) => {
   const staff = await Staff.findByIdAndUpdate(req.params, req.body, { new: true });
-  if (!staff) return res.status(404).json({ message: 'staff with the provided id does not exist' });
-  res.status(200).json(staff);
+  if (!staff) return NotFoundResponse(res);
+  sendStaff(res, staff);
 });
 
 router.delete('/:id', validate('id'), async (req, res) => {
   const staff = await Staff.findByIdAndRemove(req.params);
-  if (!staff) return res.status(404).json({ message: 'staff with the provided id does not exist' });
-  res.status(200).json(staff);
+  if (!staff) return NotFoundResponse(res);
+  sendStaff(res, staff);
 });
 
 module.exports = router;
 
-// todo: omit password before returning staff object to client
+// todo: omit password when getting all staff
 // fix status(400) message throughout the application
