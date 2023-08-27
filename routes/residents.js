913d9dc044c215cbe8e7d2bdad9f2b0ca690ab38
ankitsejwal <ankit@sejw.al>
2023-08-27 23:56:47 +0530

    omit password from response and fixes

--- routes/residents.js
+++ routes/residents.js
@@ -1,6 +1,14 @@
 const router = require('express').Router();
+const _ = require('lodash');
 const { Resident, joiSchema } = require('../models/Resident');
 const validate = require('../middleware/validate');
+
+const sendOkResponse = (res, resident) => {
+  const resident = _.omit(resident.toObject(), ['password']);
+  res.status(200).json(resident);
+};
+
+const notFoundResponse = (res) => res.status(404).json({ message: 'resident with given id did not found' });
 
 // get all the residents
 router.get('/', async (req, res) => {
@@ -11,31 +19,31 @@ router.get('/', async (req, res) => {
 // get a resident
 router.get('/:id', validate('id'), async (req, res) => {
   const resident = await Resident.findById(req.params.id);
-  if (!resident) return res.status(404).json({ message: 'resident not found' });
-  res.status(200).json(resident);
+  if (!resident) return notFoundResponse(res);
+  sendOkResponse(res, resident);
 });
 
 // add a new resident
 router.post('/', validate(joiSchema), async (req, res) => {
-  let resident = Resident.findOne({ email: req.body.email });
+  let resident = await Resident.findOne({ email: req.body.email });
   if (resident) return res.status(400).json({ message: 'Resident with this email already exists' });
   resident = new Resident(req.body);
   await resident.save();
-  res.status(200).json(resident);
+  sendOkResponse(res, resident);
 });
 
 // edit a resident
 router.put('/:id', validate('id'), validate(joiSchema), async (req, res) => {
   const resident = await Resident.findByIdAndUpdate(req.params.id, req.body, { new: true });
-  if (!resident) return res.status(404).json({ message: 'resident not found' });
-  res.status(200).json(resident);
+  if (!resident) return notFoundResponse(res);
+  sendOkResponse(res, resident);
 });
 
 // delete a resident
 router.delete('/:id', validate('id'), async (req, res) => {
   const resident = await Resident.findByIdAndRemove(req.params.id);
-  if (!resident) return res.status(404).json({ message: 'resident not found' });
-  res.status(200).json(resident);
+  if (!resident) return notFoundResponse(res);
+  sendOkResponse(res, resident);
 });
 
 module.exports = router;
