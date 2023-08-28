8e85b2b0bd5c12cfbadcf351697de8fecf2cb3ba
ankitsejwal <ankit@sejw.al>
2023-08-28 10:26:14 +0530

    authenticate users

--- models/Resident.js
+++ models/Resident.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 const Joi = require('joi');
 Joi.objectid = require('joi-objectid')(Joi);
 
---
+++ utils/genAuthToken.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = function (user) {
+  const payload = {
+    _id: user._id,
+    iat: new Date.getTime(),
+  };
+
+  const options = {
+    subject: user.role,
+    expiresIn: '24h',
+    issuer: 'mysociety',
+    audience: 'api.mysociety',
+  };
+
+  return jwt.sign(payload, 'secret', options);
+};
